70f8e506e701fc1e6c2df5946e078faa39e7388a
Adam <adam@sse.uni-hildesheim.de>
2023-06-07 17:04:19 +0200

    Actually discard compiler output, don't just not log it

--- src/main/java/net/ssehub/program_repair/geneseer/util/ProcessRunner.java
+++ src/main/java/net/ssehub/program_repair/geneseer/util/ProcessRunner.java
@@ -1,6 +1,7 @@
 package net.ssehub.program_repair.geneseer.util;
 
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +13,7 @@ public class ProcessRunner {
     
     private static final Logger LOG = Logger.getLogger(ProcessRunner.class.getName());
 
-    private int exitCode; 
+    private int exitCode;
     
     private byte[] stdout;
     
@@ -26,6 +27,7 @@ public class ProcessRunner {
         
         private long timeoutMs = -1;
         
+        private boolean captureOutput = true;
         
         public Builder(List<String> command) {
             this.command = command;
@@ -45,39 +47,59 @@ public class ProcessRunner {
             return this;
         }
         
+        public Builder captureOutput(boolean captureOutput) {
+            this.captureOutput = captureOutput;
+            return this;
+        }
+        
         public ProcessRunner run() throws IOException {
-            return new ProcessRunner(command, workingDirectory, timeoutMs);
+            return new ProcessRunner(command, workingDirectory, timeoutMs, captureOutput);
         }
         
     }
     
-    private ProcessRunner(List<String> command, Path workingDirectory, long timeoutMs) throws IOException {
+    private ProcessRunner(List<String> command, Path workingDirectory, long timeoutMs, boolean captureOutput)
+            throws IOException {
+        
         ProcessBuilder builder = new ProcessBuilder(command);
         if (workingDirectory != null) {
             builder.directory(workingDirectory.toFile());
         }
         
+        if (!captureOutput) {
+            builder.redirectOutput(Redirect.DISCARD);
+            builder.redirectError(Redirect.DISCARD);
+        }
+        
         Process process = builder.start();
         
-        Thread stdoutCapture = new Thread(() -> {
-            try {
-                this.stdout = process.getInputStream().readAllBytes();
-            } catch (IOException e) {
-                LOG.log(Level.WARNING, "Failed to read process stdout", e);
-            }
-        }, "stdout-capture");
-        Thread stderrCapture = new Thread(() -> {
-            try {
-                this.stderr = process.getErrorStream().readAllBytes();
-            } catch (IOException e) {
-                LOG.log(Level.WARNING, "Failed to read process stderr", e);
-            }
-        }, "stderr-capture");
-        
-        stdoutCapture.setDaemon(true);
-        stderrCapture.setDaemon(true);
-        stdoutCapture.start();
-        stderrCapture.start();
+        Thread stdoutCapture;
+        Thread stderrCapture;
+        if (captureOutput) {
+            stdoutCapture = new Thread(() -> {
+                try {
+                    this.stdout = process.getInputStream().readAllBytes();
+                } catch (IOException e) {
+                    LOG.log(Level.WARNING, "Failed to read process stdout", e);
+                }
+            }, "stdout-capture");
+            stderrCapture = new Thread(() -> {
+                try {
+                    this.stderr = process.getErrorStream().readAllBytes();
+                } catch (IOException e) {
+                    LOG.log(Level.WARNING, "Failed to read process stderr", e);
+                }
+            }, "stderr-capture");
+            
+            stdoutCapture.setDaemon(true);
+            stderrCapture.setDaemon(true);
+            stdoutCapture.start();
+            stderrCapture.start();
+            
+        } else {
+            stdoutCapture = null;
+            stderrCapture = null;
+        }
 
         if (timeoutMs > 0) {
             boolean terminated = untilNoInterruptedException(() -> process.waitFor(timeoutMs, TimeUnit.MILLISECONDS));
@@ -97,14 +119,16 @@ public class ProcessRunner {
             this.exitCode = untilNoInterruptedException(() -> process.waitFor());
         }
         
-        untilNoInterruptedException(() -> {
-            stdoutCapture.join();
-            return null;
-        });
-        untilNoInterruptedException(() -> {
-            stderrCapture.join();
-            return null;
-        });
+        if (captureOutput) {
+            untilNoInterruptedException(() -> {
+                stdoutCapture.join();
+                return null;
+            });
+            untilNoInterruptedException(() -> {
+                stderrCapture.join();
+                return null;
+            });
+        }
     }
     
     public interface Interruptable<T> {

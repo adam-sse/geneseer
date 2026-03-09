30fbb6ce30dcb07252bedf385ba7699d742b1334
Adam Krafczyk <krafczyk@uni-hildesheim.de>
2026-03-09 10:02:37 +0100

    Refactor: Remove unused constructor

--- src/main/java/net/ssehub/program_repair/geneseer/llm/AnswerDoesNotApplyException.java
+++ src/main/java/net/ssehub/program_repair/geneseer/llm/AnswerDoesNotApplyException.java
@@ -5,10 +5,6 @@ class AnswerDoesNotApplyException extends Exception {
 
     private static final long serialVersionUID = -7304181087872893564L;
 
-    public AnswerDoesNotApplyException() {
-        super();
-    }
-
     public AnswerDoesNotApplyException(String message) {
         super(message);
     }
